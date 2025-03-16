3bd1ed686345e875fb3da51c54f8ef6673e97329
Loreto <loreto0105@gmail.com>
2025-03-16 17:07:08 +0100

    Deployed new version of the page to production

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,2 +1,6 @@
-console.log(">> Ready :)");
+console.log(">> Ready :)");Input.value;const c=document.querySelector(".js_searchBttn"),r=document.querySelector(".js_results"),u=i=>{i.preventDefault();const n=Input.value;if(n!=""){const l=`https://api.jikan.moe/v4/anime?q=${n}`;fetch(l).then(e=>e.json()).then(e=>{console.log(e);const o=e.data;let s="";for(let t of o)s+=`
+                    <li>
+                        <div> <img src="${t.images.jpg.image_url}" alt="${t.title}" /> </div>
+                        <h2>${t.title}</h2>
+                    </li>`;r.innerHTML=s})}else alert("Aquí puedes introducir tu búsqueda.")};c.addEventListener("click",u);
 //# sourceMappingURL=main.js.map
