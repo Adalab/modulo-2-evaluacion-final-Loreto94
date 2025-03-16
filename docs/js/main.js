2bf92158845bb90d1fa5f69735b4f8409acfcb1d
Loreto <loreto0105@gmail.com>
2025-03-16 17:09:19 +0100

    Deployed new version of the page to production

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,6 +1,6 @@
-console.log(">> Ready :)");Input.value;const c=document.querySelector(".js_searchBttn"),r=document.querySelector(".js_results"),u=i=>{i.preventDefault();const n=Input.value;if(n!=""){const l=`https://api.jikan.moe/v4/anime?q=${n}`;fetch(l).then(e=>e.json()).then(e=>{console.log(e);const o=e.data;let s="";for(let t of o)s+=`
+console.log(">> Ready :)");const o=document.querySelector(".js_input");o.value;const r=document.querySelector(".js_searchBttn"),u=document.querySelector(".js_results"),a=c=>{c.preventDefault();const n=o.value;if(n!=""){const i=`https://api.jikan.moe/v4/anime?q=${n}`;fetch(i).then(e=>e.json()).then(e=>{console.log(e);const l=e.data;let s="";for(let t of l)s+=`
                     <li>
                         <div> <img src="${t.images.jpg.image_url}" alt="${t.title}" /> </div>
                         <h2>${t.title}</h2>
-                    </li>`;r.innerHTML=s})}else alert("Aquí puedes introducir tu búsqueda.")};c.addEventListener("click",u);
+                    </li>`;u.innerHTML=s})}else alert("Aquí puedes introducir tu búsqueda.")};r.addEventListener("click",a);
 //# sourceMappingURL=main.js.map
