65ae39db050e28f62809f29dbe255803ae2027b7
Loreto <loreto0105@gmail.com>
2025-03-17 11:15:21 +0100

    Deployed new version of the page to production

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,6 +1,6 @@
-console.log(">> Ready :)");const o=document.querySelector(".js_input");o.value;const r=document.querySelector(".js_searchBttn"),u=document.querySelector(".js_results"),a=c=>{c.preventDefault();const n=o.value;if(n!=""){const i=`https://api.jikan.moe/v4/anime?q=${n}`;fetch(i).then(e=>e.json()).then(e=>{console.log(e);const l=e.data;let s="";for(let t of l)s+=`
-                    <li>
+console.log(">> Ready :)");const l=document.querySelector(".js_input");l.value;const u=document.querySelector(".js_searchBttn"),c=document.querySelector(".js_results");let o=[];const d=i=>{i.preventDefault();const n=l.value;if(n!=""){const s=`https://api.jikan.moe/v4/anime?q=${n}`;fetch(s).then(e=>e.json()).then(e=>{console.log(e),o=e.data;let r="";for(let t of o)r+=`
+                    <li class="js_serie" id=${t.mal_id}>
                         <div> <img src="${t.images.jpg.image_url}" alt="${t.title}" /> </div>
                         <h2>${t.title}</h2>
-                    </li>`;u.innerHTML=s})}else alert("Aquí puedes introducir tu búsqueda.")};r.addEventListener("click",a);
+                    </li>`;c.innerHTML=r;const a=document.querySelectorAll(".js_serie");for(const t of a)t.addEventListener("click",m)})}else n===""&&(c.innerHTML="")};u.addEventListener("click",d);function m(i){console.log("Ha hecho click en una serie favorita"),console.log(i.currentTarget.id);const n=i.currentTarget.id,s=o.find(e=>e.mal_id===n);console.log(s)}
 //# sourceMappingURL=main.js.map
