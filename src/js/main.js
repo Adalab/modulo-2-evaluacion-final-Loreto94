3acc6bdb0e1cecdc9dae4d428eb8d3826169957e
Loreto <loreto0105@gmail.com>
2025-03-17 13:53:32 +0100

    Results

--- src/js/main.js
+++ src/js/main.js
@@ -8,6 +8,8 @@ const searchButton = document.querySelector(".js_searchBttn");
 const results = document.querySelector(".js_results");
 let favorites = [];
 let seriesAnime = [];
+let sectionFavoritesHTML = document.querySelector(".js_favorites");
+
 
 const fetchSeries = (event) => {
     event.preventDefault();
@@ -37,12 +39,6 @@ const fetchSeries = (event) => {
     } else if (inputText === "") {
         results.innerHTML = "";
     }
-
-
-
-
-
-
 }
 
 searchButton.addEventListener("click", fetchSeries);
@@ -51,12 +47,33 @@ function handleAddFavorite(event) {
     console.log("Ha hecho click en una serie favorita");
     console.log(event.currentTarget.id);
     const idSerieAnimeFav = event.currentTarget.id;
+    console.log(seriesAnime);
 
     const serieAnimeSelected = seriesAnime.find((serie) => {
-        return serie.mal_id === idSerieAnimeFav;
+        console.log(serie.mal_id);
+        console.log(serie);
+        console.log(idSerieAnimeFav);
+        return serie.mal_id === parseInt(idSerieAnimeFav); //convertir idSeriesAnimeFav a número
     })
     console.log(serieAnimeSelected);
+
+    favorites.push(serieAnimeSelected); //Añado elementos al nuevo Array
+    
+    function handleAddFavorite () {
+        sectionFavoritesHTML.innerHTML = "";
+            for (serie of favorites) {
+                let content = `
+                    <li class="js_serie" id=${serie.mal_id}>
+                        <div> <img src="${serie.images.jpg.image_url}" alt="${serie.title}" /> </div>
+                        <h2>${serie.title}</h2>
+                    </li>`;
+        sectionFavoritesHTML.innerHTML += content;
+    }
 }
+    
+serieAnimeFav.addEventListener("clik", handleAddFavorite);
+}
+
 
 
 
